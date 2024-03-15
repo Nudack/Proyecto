45776671a94c31391b817a5e0a443de24341d8c6
Nudack <patriciosoriabenitez@gmail.com>
2024-03-15 13:16:05 +0000

    LoginScreen
    Creación del proyecto en Android Studio, creando la pantalla de inicio de sesión de la aplicación

---
+++ app/src/main/java/com/example/hospedafcil/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.hospedafcil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import com.example.hospedafcil.ui.theme.HospedaFácilTheme
+import com.example.hospedafcil.ui.theme.login.ui.LoginScreen
+import com.example.hospedafcil.ui.theme.login.viewModel.LoginViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HospedaFácilTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    //crear NavHost
+
+                    var cambiarPantalla = rememberSaveable { mutableStateOf(true) }
+                    if (cambiarPantalla.value){
+                    LoginScreen(LoginViewModel(), continuar = {cambiarPantalla.value = false})
+                    }else{
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hospedafcil/ui/theme/login/ui/LoginScreen.kt
@@ -0,0 +1,87 @@
+package com.example.hospedafcil.ui.theme.login.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import com.example.hospedafcil.R
+import com.example.hospedafcil.ui.theme.login.viewModel.LoginViewModel
+
+@Composable
+fun LoginScreen (
+    viewModel: LoginViewModel,
+    continuar: () -> Unit
+) {
+    Column (
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val email: String by viewModel.email.observeAsState(initial = "")
+        val password: String by viewModel.password.observeAsState(initial = "")
+        val loginEnable: Boolean by viewModel.loginEnable.observeAsState(initial = false)
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "inicio de sesion",
+            Modifier.size(250.dp)
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        EmailField(email) { viewModel.onLoginChanged(it, password) }
+        Spacer(modifier = Modifier.padding(8.dp))
+        PasswordField(password) { viewModel.onLoginChanged(email, it) }
+        Spacer(modifier = Modifier.padding(16.dp))
+        if (!loginEnable){
+            Text(text = "El correo o la contraseña no son correctas", color = Color.Red)
+        }
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(onClick = { continuar() },
+            enabled = loginEnable) {
+            Text(text = "Iniciar Sesión")
+        }
+    }
+}
+
+@Composable
+fun PasswordField(password: String, onTextFieldChanged: (String) -> Unit) {
+    TextField(
+        value = password, onValueChange = { onTextFieldChanged(it) },
+        placeholder = { Text(text = "Contraseña") },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = PasswordVisualTransformation(),
+        singleLine = true,
+        maxLines = 1,
+    )
+}
+
+@Composable
+fun EmailField(email: String, onTextFieldChanged: (String) -> Unit) {
+    TextField(
+        value = email, onValueChange = { onTextFieldChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+    )
+}
